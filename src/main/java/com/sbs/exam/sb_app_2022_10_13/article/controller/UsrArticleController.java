e61351e87b663d08b7814f3389f2beb56988aa3e
SangWon7242 <gotkdqja@gmail.com>
2022-10-20 17:08:17 +0900

    getArticle 액션 구현

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/article/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/article/controller/UsrArticleController.java
@@ -82,6 +82,18 @@ public class UsrArticleController {
     return articles;
   }
 
+  @RequestMapping("/usr/article/getArticle")
+  @ResponseBody
+  public Object getArticleAction(int id) {
+    Article article = getArticle(id);
+
+    if ( article == null ) {
+      return id + "번 게시물이 존재하지 않습니다.";
+    }
+
+    return article;
+  }
+
   @RequestMapping("/usr/article/doDelete")
   @ResponseBody
   public String doDelete(int id) {
