7c1724a91c0e3a51b1feb6e5c7715b0691a2b287
SangWon7242 <gotkdqja@gmail.com>
2022-10-21 16:48:16 +0900

    DB 스키마 생성, 게시물 테이블 생성, DB 접속 정보 수정

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+# DB 생성
+DROP DATABASE IF EXISTS sb_app_2022_t;
+CREATE DATABASE sb_app_2022_t;
+USE sb_app_2022_t;
+
+# 게시물 테이블 생성
+CREATE TABLE article (
+	id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	regDate DATETIME NOT NULL,
+	updateDate DATETIME NOT NULL,
+	title CHAR(100) NOT NULL,
+	`body` TEXT NOT NULL
+);
+
+# 게시물, 테스트 데이터 생성
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목1',
+`body` = '내용1';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목2',
+`body` = '내용2';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목3',
+`body` = '내용3';
