06a4821e4b0b31c6b0bdda9d2207ddc436de9da0
SangWon7242 <gotkdqja@gmail.com>
2022-10-25 17:53:08 +0900

    MemberRepository 추가, 회원가입 처리

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/member/repository/MemberRepository.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/member/repository/MemberRepository.java
@@ -1,4 +1,22 @@
 package com.sbs.exam.sb_app_2022_10_13.member.repository;
 
-public class MemberRepository {
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface MemberRepository {
+  @Insert("""
+          INSERT INTO `member`
+          SET regDate = NOW(),
+          updateDate = NOW(),
+          loginId = #{loginId},
+          loginPw = #{loginPw},
+          `name` = #{name},
+          `nickname` = #{nickname},
+          cellphoneNo = #{cellphoneNo},
+          email = #{email}
+          """)
+  public void join(@Param("loginId") String loginId, @Param("loginPw") String loginPw, @Param("name") String name,
+                   @Param("nickname") String nickname, @Param("cellphoneNo") String cellphoneNo, @Param("email") String email);
 }
--- src/main/java/com/sbs/exam/sb_app_2022_10_13/member/service/MemberService.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/member/service/MemberService.java
@@ -6,9 +6,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MemberService {
-  // private MemberRepository memberRepository;
+  private MemberRepository memberRepository;
+
+  public MemberService(MemberRepository memberRepository) {
+    this.memberRepository = memberRepository;
+  }
 
   public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
-    // memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+    memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
   }
 }
