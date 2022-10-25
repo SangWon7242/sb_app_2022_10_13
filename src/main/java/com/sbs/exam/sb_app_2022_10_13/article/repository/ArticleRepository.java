bf80fe2f6109db4fbc13dd38a80a8f860f80025b
SangWon7242 <gotkdqja@gmail.com>
2022-10-25 16:26:55 +0900

    게시물 수정시, 입력된 정보만 수정

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/article/repository/ArticleRepository.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/article/repository/ArticleRepository.java
@@ -8,20 +8,52 @@ import java.util.List;
 @Mapper
 public interface ArticleRepository {
 
-  @Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = #{title}, `body` = #{body}")
-  public void writeArticle(String title, String body);
+  @Insert("""
+          INSERT INTO article
+          SET regDate = NOW(),
+          updateDate = NOW(),
+          title = #{title},
+          `body` = #{body}
+          """)
+  public void writeArticle(@Param("title") String title, @Param("body") String body);
 
-  @Select("SELECT * FROM article WHERE id = #{id}")
-  public Article getArticle(int id);
+  @Select("""
+          SELECT *
+          FROM article
+          WHERE id = #{id}
+          """)
+  public Article getArticle(@Param("id") int id);
 
-  @Delete("DELETE FROM article WHERE id = #{id}")
-  public void deleteArticle(int id);
+  @Delete("""
+          DELETE
+          FROM article
+          WHERE id = #{id}
+          """)
+  public void deleteArticle(@Param("id") int id);
 
-  @Select("SELECT * FROM article ORDER BY id DESC")
+  @Select("""
+          SELECT *
+          FROM article
+          ORDER BY id DESC
+          """)
   public List<Article> getArticles();
 
-  @Update("UPDATE article SET title = #{title}, `body` = #{body}, updateDate = NOW() WHERE id = #{id}")
-  public void modifyArticle(int id, String title, String body);
+  @Update("""
+          <script>
+          UPDATE article
+          <set>
+            <if test='title != null'>
+              title = #{title},
+            </if>
+            <if test='body != null'>
+              `body` = #{body},
+            </if>
+            updateDate = NOW()
+          </set>
+          WHERE id = #{id}
+          </script>
+          """)
+  public void modifyArticle(@Param("id")int id, @Param("title") String title, @Param("body") String body);
 
   @Select("SELECT LAST_INSERT_ID()")
   public int getLastInsertId();
