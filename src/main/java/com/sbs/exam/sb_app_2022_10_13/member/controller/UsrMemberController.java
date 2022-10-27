4b3164514f6903c5ab59133fe155f2ba00824cc3
SangWon7242 <gotkdqja@gmail.com>
2022-10-27 16:22:37 +0900

    회원 가입 완료 후, 생성된 회원 보여줌

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/member/controller/UsrMemberController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/member/controller/UsrMemberController.java
@@ -1,6 +1,7 @@
 package com.sbs.exam.sb_app_2022_10_13.member.controller;
 
 import com.sbs.exam.sb_app_2022_10_13.member.service.MemberService;
+import com.sbs.exam.sb_app_2022_10_13.member.vo.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,10 +17,13 @@ public class UsrMemberController {
 
   @RequestMapping("/usr/member/doJoin")
   @ResponseBody
-  public String doJoin(String loginId, String loginPw, String name, String nickname,
+  public Member doJoin(String loginId, String loginPw, String name, String nickname,
                        String cellphoneNo, String email) {
-    memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
-    return "성공";
+    int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+
+    Member member = memberService.getMemberById(id);
+
+    return member;
   }
 
 }
