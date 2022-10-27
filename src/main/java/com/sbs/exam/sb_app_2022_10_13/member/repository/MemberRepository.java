9759967883806127adc5b6168e1f1425716b41c6
SangWon7242 <gotkdqja@gmail.com>
2022-10-27 16:39:01 +0900

    회원 가입시, 로그인 아이디 중복 체크

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/member/repository/MemberRepository.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/member/repository/MemberRepository.java
@@ -33,4 +33,11 @@ public interface MemberRepository {
           WHERE M.id = #{id}
           """)
   Member getMemberById(@Param("id") int id);
+
+  @Select("""
+          SELECT *
+          FROM `member` AS M
+          WHERE M.loginId = #{loginId}
+          """)
+  Member getMemberByLoginId(String loginId);
 }
