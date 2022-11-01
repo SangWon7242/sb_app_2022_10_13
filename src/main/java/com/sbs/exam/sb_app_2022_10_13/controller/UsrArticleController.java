a9ec1fd569122c1201ecdb942feb681f2b03af73
SangWon7242 <gotkdqja@gmail.com>
2022-11-01 17:11:16 +0900

    C:/Program Files/Git/usr/article/getArticles에 ResultData 적용

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/controller/UsrArticleController.java
@@ -38,8 +38,10 @@ public class UsrArticleController {
 
   @RequestMapping("/usr/article/getArticles")
   @ResponseBody
-  public List<Article> getArticles() {
-    return articleService.getArticles();
+  public ResultData getArticles() {
+    List<Article> articles = articleService.getArticles();
+
+    return ResultData.from("S-1", "게시물 리스트입니다.", articles);
   }
 
   @RequestMapping("/usr/article/getArticle")
