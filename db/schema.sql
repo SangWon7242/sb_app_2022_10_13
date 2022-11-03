3336e87c3c531ebbb47367d75ceb0896ad7f6949
SangWon7242 <gotkdqja@gmail.com>
2022-11-03 17:18:35 +0900

    게시물 삭제시 권한체크

--- db/schema.sql
+++ db/schema.sql
@@ -75,12 +75,15 @@ SET regDate = NOW(),
 updateDate = NOW(),
 loginId = 'user2',
 loginPw = 'user2',
-`name` = 'user1',
-`nickname` = 'user1',
+`name` = 'user2',
+`nickname` = 'user2',
 cellphoneNo = '01011111111',
 email = 'gotkdqja@gmail.com';
 
-SELECT * FROM `member`;
-
 # 게시물 테이블에 회원정보 추가
 ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER `updateDate`;
+
+# 기존 게시물의 작성자를 2번으로 지정
+UPDATE article
+SET memberId = 2
+WHERE memberId = 0;
