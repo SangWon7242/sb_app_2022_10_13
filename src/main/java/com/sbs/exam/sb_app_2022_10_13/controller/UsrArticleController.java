c37b650fbee310b1b377d83edded6d27c99d3cfc
SangWon7242 <gotkdqja@gmail.com>
2022-11-03 17:13:02 +0900

    게시물 작성시, 로그인 여부 체크, 작성자 정보 저장

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/controller/UsrArticleController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -18,7 +19,20 @@ public class UsrArticleController {
 
   @RequestMapping("/usr/article/doAdd")
   @ResponseBody
-  public ResultData<Article> doAdd(String title, String body) {
+  public ResultData<Article> doAdd(HttpSession httpSession, String title, String body) {
+    boolean isLogined = false;
+    int loginedMemberId = 0;
+
+    if (httpSession.getAttribute("loginedMemberId") != null ) {
+      isLogined = true;
+      loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
+    }
+
+    if ( isLogined == false ) {
+      return ResultData.from("F-A", "로그인 후 이용해주세요.");
+    }
+
+
     if( Ut.empty(title) ) {
       return ResultData.from("F-1", "title(을)를 입력해주세요.");
     }
@@ -27,7 +41,7 @@ public class UsrArticleController {
       return ResultData.from("F-2", "body(을)를 입력해주세요.");
     }
 
-    ResultData<Integer> writeArticleRd = articleService.writeArticle(title, body);
+    ResultData<Integer> writeArticleRd = articleService.writeArticle(loginedMemberId, title, body);
     int id = writeArticleRd.getData1();
 
     Article article = articleService.getArticle(id);
