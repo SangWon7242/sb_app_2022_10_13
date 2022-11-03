60189660d85eb8ed21eb1285168dcc091eac1519
SangWon7242 <gotkdqja@gmail.com>
2022-11-03 16:34:02 +0900

    로그인 기능 구현

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/service/MemberService.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/service/MemberService.java
@@ -37,11 +37,11 @@ public class MemberService {
     return ResultData.from("S-1", "회원 가입이 완료 되었습니다.", id);
   }
 
-  private Member getMemberByNameAndEmail(String name, String email) {
+  public Member getMemberByNameAndEmail(String name, String email) {
     return memberRepository.getMemberByNameAndEmail(name, email);
   }
 
-  private Member getMemberByLoginId(String loginId) {
+  public Member getMemberByLoginId(String loginId) {
     return memberRepository.getMemberByLoginId(loginId);
   }
 
