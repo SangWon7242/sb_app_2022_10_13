2b16a58737f004120ff0bda66c4b8130a2e6d6e5
SangWon7242 <gotkdqja@gmail.com>
2022-11-04 16:32:06 +0900

    게시물 리스트, JSP로 구현

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/controller/UsrArticleController.java
@@ -6,6 +6,7 @@ import com.sbs.exam.sb_app_2022_10_13.vo.Article;
 import com.sbs.exam.sb_app_2022_10_13.vo.ResultData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -49,12 +50,13 @@ public class UsrArticleController {
     return ResultData.newData(writeArticleRd, "article", article);
   }
 
-  @RequestMapping("/usr/article/getArticles")
-  @ResponseBody
-  public ResultData<List<Article>> getArticles() {
+  @RequestMapping("/usr/article/list")
+  public String showList(Model model) {
     List<Article> articles = articleService.getArticles();
 
-    return ResultData.from("S-1", "게시물 리스트입니다.", "articles", articles);
+    model.addAttribute("articles", articles);
+
+    return "usr/article/list";
   }
 
   @RequestMapping("/usr/article/getArticle")
