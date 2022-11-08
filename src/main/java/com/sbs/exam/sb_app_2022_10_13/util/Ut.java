244bfee968e2fbcd33fbe442b117848c37031fbd
SangWon7242 <gotkdqja@gmail.com>
2022-11-08 17:18:48 +0900

    게시물 삭제 후 replace로 이동

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/util/Ut.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/util/Ut.java
@@ -2,11 +2,11 @@ package com.sbs.exam.sb_app_2022_10_13.util;
 
 public class Ut {
   public static boolean empty(Object obj) {
-    if(obj == null) {
+    if (obj == null) {
       return true;
     }
 
-    if( obj instanceof String == false ) {
+    if (obj instanceof String == false) {
       return true;
     }
 
@@ -17,5 +17,41 @@ public class Ut {
 
   public static String f(String format, Object... args) {
     return String.format(format, args);
+  }
+
+  public static String jsHistoryBack(String msg) {
+    if (msg == null) {
+      msg = "";
+    }
+
+    return Ut.f("""
+        <script>
+        const msg = '%s'.trim();
+        if( msg.length > 0) {
+           alert(msg);
+        }           
+        history.back();
+        </script>
+        """, msg);
+  }
+
+  public static String jsReplace(String msg, String uri) {
+    if (msg == null) {
+      msg = "";
+    }
+
+    if (uri == null) {
+      uri = "";
+    }
+
+      return Ut.f("""
+          <script>
+          const msg = '%s'.trim();
+          if( msg.length > 0) {
+             alert(msg);
+          }        
+          location.replace('%s');            
+          </script>
+          """, msg, uri);
   }
 }
