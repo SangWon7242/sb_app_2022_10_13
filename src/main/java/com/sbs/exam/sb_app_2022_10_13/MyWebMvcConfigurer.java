065e058c5a2e3b5c1a9dbdd99d1ecab2b477973f
SangWon7242 <gotkdqja@gmail.com>
2022-11-10 16:25:46 +0900

    BeforAction 인터셉터 등록

---
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/MyWebMvcConfigurer.java
@@ -0,0 +1,24 @@
+package com.sbs.exam.sb_app_2022_10_13;
+
+import com.sbs.exam.sb_app_2022_10_13.interceptor.BeforeActionInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyWebMvcConfigurer implements WebMvcConfigurer {
+  // beforeActionInterceptor 인터셉터 불러오기
+  @Autowired
+  BeforeActionInterceptor beforeActionInterceptor;
+
+  // 이 함수는 인터셉터를 적용하는 역할을 합니다.
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(beforeActionInterceptor)
+        .addPathPatterns("/**")
+        .excludePathPatterns("/css/**")
+        .excludePathPatterns("/js/**")
+        .excludePathPatterns("/error");
+  }
+}
