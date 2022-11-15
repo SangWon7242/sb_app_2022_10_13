43127f850659934208c1bc0b7eb3a4a8a125b952
SangWon7242 <gotkdqja@gmail.com>
2022-11-15 16:15:19 +0900

    로그아웃 처리에 Rq 적용

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/vo/Rq.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/vo/Rq.java
@@ -65,4 +65,8 @@ public class Rq {
   public void login(Member member) {
     session.setAttribute("loginedMemberId", member.getId());
   }
+
+  public void logout() {
+    session.removeAttribute("loginedMemberId");
+  }
 }
