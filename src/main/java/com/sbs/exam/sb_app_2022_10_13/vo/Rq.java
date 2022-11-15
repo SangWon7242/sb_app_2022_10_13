3d73db55f82e155465b2db7d3a2c3157638390fe
SangWon7242 <gotkdqja@gmail.com>
2022-11-15 16:36:37 +0900

    게시물 수정폼을 보여주기전에 체크

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/vo/Rq.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/vo/Rq.java
@@ -38,16 +38,7 @@ public class Rq {
 
   public void printHistoryBackJs(String msg) {
     resp.setContentType("text/html; charset=UTF-8");
-
-    println("<script>");
-
-    if(!Ut.empty(msg)) {
-      println("alert('" + msg + "');");
-    }
-
-    println("history.back();");
-
-    println("</script>");
+    print(Ut.jsHistoryBack(msg));
   }
 
   public void print(String str) {
@@ -68,5 +59,11 @@ public class Rq {
 
   public void logout() {
     session.removeAttribute("loginedMemberId");
+  }
+
+  public String historyBackJsOnView(String msg) {
+    req.setAttribute("msg", msg);
+    req.setAttribute("historyBack", true);
+    return "common/js";
   }
 }
