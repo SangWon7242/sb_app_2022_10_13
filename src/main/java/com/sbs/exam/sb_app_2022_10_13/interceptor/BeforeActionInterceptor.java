75b8989d628dd23935ececb5d1f2d6677bba9337
SangWon7242 <gotkdqja@gmail.com>
2022-11-18 17:11:28 +0900

    beforeActionInterceptor에서 Rq 객체를 좀 더 확실하게 생성하도록 유도

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/interceptor/BeforeActionInterceptor.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/interceptor/BeforeActionInterceptor.java
@@ -13,9 +13,15 @@ import javax.servlet.http.HttpServletResponse;
 
 @Component
 public class BeforeActionInterceptor implements HandlerInterceptor {
+  private Rq rq;
+
+  public BeforeActionInterceptor(Rq rq) {
+    this.rq = rq;
+  }
+
   @Override
   public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handle) throws Exception {
-    // 이제는 Rq 객체가 자동으로 만들어지기 때문에 필요 없음
+    rq.initOnBeforeActionInterceptor();
 
     return HandlerInterceptor.super.preHandle(req, resp, handle);
   }
