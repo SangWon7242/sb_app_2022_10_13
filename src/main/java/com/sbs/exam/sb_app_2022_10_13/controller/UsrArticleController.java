89aba9c24186963c53c0985139dfdbdfcbe19959
SangWon7242 <gotkdqja@gmail.com>
2022-11-24 17:20:01 +0900

    조회수 증가 성공에 대한 답장에, data2(글 번호) 정보 추가

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/controller/UsrArticleController.java
@@ -102,7 +102,11 @@ public class UsrArticleController {
       return increaseHitCountRd;
     }
 
-    return ResultData.newData(increaseHitCountRd, "hitCount", articleService.getArticleHitCount(id));
+    ResultData<Integer> rd = ResultData.newData(increaseHitCountRd, "hitCount", articleService.getArticleHitCount(id));
+
+    rd.setData2("id", id);
+
+    return rd;
   }
 
 
